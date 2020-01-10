1d82106b120aab7907d44c658347970773867e61
nurfadhilah.ramadhan <nurfadhilahramadhann@gmail.com>
2020-01-10 10:40:22 +0700

    Finish refactor module profile

--- app.js
+++ app.js
@@ -1,37 +1,5 @@
-let https = require('https')
-const username = "HasbiAlfaridzi"
-const options = {
-    hostname: 'api.github.com',
-    port: 443,
-    path: `/users/${username}`,
-    method: 'GET',
-    headers: {
-        'user-agent': 'nodejs'
-    }
-}
+let profile = require('./profile')
 
-let request = https.request(options, (response) => {
-    let body = ''
-    response.on('data', (data) => {
-        body = body + data
-    })
-    response.on('end', () => {
-        let profile = JSON.parse(body)
-        if (response.statusCode === 200) {
-            console.log(`${profile.login} owns ${profile.public_repos} repo(s) and has ${profile.followers} followers`)
-        }
-        else {
-            console.log(`Profile with username '${username}' not found`)
-        }
-
-
-
-    })
-
-})
-
-request.end()
-
-request.on('error', (e) => {
-    console.error(e)
-})
+profile.get('HasbiAlfaridzi')
+profile.get('yofri')
+profile.get('ronishak')
---
+++ profile.js
@@ -0,0 +1,43 @@
+let https = require('https')
+
+let get = (username) => {
+
+    const options = {
+        hostname: 'api.github.com',
+        port: 443,
+        path: `/users/${username}`,
+        method: 'GET',
+        headers: {
+            'user-agent': 'nodejs'
+        }
+    }
+
+    let request = https.request(options, (response) => {
+        let body = ''
+        response.on('data', (data) => {
+            body = body + data
+        })
+        response.on('end', () => {
+            let profile = JSON.parse(body)
+            if (response.statusCode === 200) {
+                console.log(`${profile.login} owns ${profile.public_repos} repo(s) and has ${profile.followers} followers`)
+            }
+            else {
+                console.log(`Profile with username '${username}' not found`)
+            }
+
+
+
+        })
+
+    })
+
+    request.end()
+
+    request.on('error', (e) => {
+        console.error(e)
+    })
+}
+
+module.exports = { get }
+
